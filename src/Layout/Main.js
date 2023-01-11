3b0b440c756f8fc3d293bb6c963c150023d6179d
kazirofiq <rofiqul01941056960@gmail.com>
2023-01-11 14:24:41 +0600

    navbar

---
+++ src/Layout/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBar from '../Share/NavBar/NavBar';
+
+const Main = () => {
+    return (
+        <div>
+            <NavBar></NavBar>
+        </div>
+    );
+};
+
+export default Main;
